52034bb0c831bdd23f272a3571129628d08a71f7
opqieubdo <jeremy.evan26@gmail.com>
2025-02-12 00:49:28 +0000

    Submit

---
+++ poytuy/tuklt.py
@@ -0,0 +1,9 @@
+# input distance and time value
+distance = float(input('enter the distance (in meters): '))
+time = float(input('enter the time (in seconds): ' ))
+
+# formula for velocity
+velocity = distance / time 
+
+# printing the output/the value of velocity
+print(f'the velocity is {velocity:.2f} m/s.')
